1173a76261372470864ad0bc81d39605579558e3
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2022-05-14 16:38:37 +0200

    introduction to styling

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function ContactPage() {
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <Link href="/">
+        <a>Home</a>
+      </Link>
+    </div>
+  );
+}
---
+++ pages/image.js
@@ -0,0 +1,8 @@
+export default function ImagePage() {
+  return (
+    <div>
+      <h1>Next.js Logo</h1>
+      <img src="/logo.png" alt="Logo" width={128} height={128} />;
+    </div>
+  );
+}
